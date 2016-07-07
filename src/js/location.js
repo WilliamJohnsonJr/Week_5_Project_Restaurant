522bbef37839e89d2b4bee417170367a95eb9950
humdrum <jjrajani@gmail.com>
2016-07-07 17:07:26 -0400

    placed location div content. needs style

--- src/js/location.js
+++ src/js/location.js
@@ -2,10 +2,10 @@ import $ from 'jquery'
 
 var locationHTML = `<p>Our Location</p>
  <iframe height="100px" frameborder="0" style="border:0"
-src="https://www.google.com/maps/embed/v1/place?q=-0.104665%2C%2032.701386&key=AIzaSyASfoADlEq3KS_EdhjcOgCnN4WuEhvrE0c" allowfullscreen></iframe>
- <p>Address</p>
+src="https://www.google.com/maps/embed/v1/place?q=0.296143%2C%2032.621606&key=AIzaSyASfoADlEq3KS_EdhjcOgCnN4WuEhvrE0c" allowfullscreen></iframe>
+ <p class="address">Kalungi Rd, Muyenga, Kampala</p>
  <ul>
-  <li>555-555-1234</li>
+  <li class="first">555-555-1234</li>
   <li>555-555-1234</li>
   <li><a href=#>Map</a></li>
   <li><a href=#>Directions</a></li>
